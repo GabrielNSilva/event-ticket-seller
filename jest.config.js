3559a19a849af1800451d9e5862fc83a53a81cf9
Gabriel NS <gabriel.nascimento@dasa.com.br>
2022-03-04 11:57:55 -0300

    refactor: removing double quote rule from eslint

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  preset: "ts-jest",
-  roots: ["<rootDir>/test"],
-  coverageDirectory: "coverage",
+  preset: 'ts-jest',
+  roots: ['<rootDir>/test'],
+  coverageDirectory: 'coverage',
   collectCoverageFrom: [
-    "<rootDir>/src/**/*.ts",
-    "!**/test/**",
-    "!**/config/**",
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**',
   ],
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/src/$1',
     // "^@test/(.*)$": "<rootDir>/test/$1",
   },
-  setupFiles: ["dotenv/config"],
+  setupFiles: ['dotenv/config'],
 };
